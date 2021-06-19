cc3fda4470c17e514118a34e2b5716263710cd24
SAch <59020885+Shirshak-Ach@users.noreply.github.com>
2021-06-19 21:59:55 +0545

    Bottom Navigation bar with drawer

--- lib/Screen/BottomNavigation.dart
+++ lib/Screen/BottomNavigation.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hamro_chitchat/Screen/HomeScreen.dart';
 import 'package:hamro_chitchat/Screen/Search.dart';
@@ -10,7 +11,6 @@ class BottomNavigation extends StatefulWidget {
 
 class _BottomNavigationState extends State<BottomNavigation> {
   int _currentIndex = 0;
-
   final tabs = [
     HomeScreen(),
     SearchScreen(),
@@ -28,13 +28,8 @@ class _BottomNavigationState extends State<BottomNavigation> {
             },
             icon: Icon(Icons.home)),
         title: Text("Hamro ChitChat"),
-        actions: [
-          IconButton(
-              onPressed: () => logOut(context),
-              tooltip: 'LogOut',
-              icon: Icon(Icons.logout)),
-        ],
       ),
+      endDrawer: MyDrawer(),
       body: tabs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
@@ -62,3 +57,106 @@ class _BottomNavigationState extends State<BottomNavigation> {
     );
   }
 }
+
+class MyDrawer extends StatelessWidget {
+  final Function onTap;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  MyDrawer({this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[300]),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      height: 50,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        backgroundImage: NetworkImage(
+                            'https://www.naps.com.au/media/1060/user-icon-placeholder-1.png?width=360&height=400&anchor=center&mode=crop'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      _auth.currentUser.displayName,
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      _auth.currentUser.email,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Ink(
+              color: Colors.grey[200],
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.blue,
+                ),
+                title: Text('Home'),
+                onTap: () => Navigator.pop(context),
+              ),
+            ),
+            Divider(
+              height: 5,
+            ),
+            Ink(
+              color: Colors.grey[200],
+              child: ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.blue,
+                ),
+                title: Text('Settings'),
+                onTap: () => Navigator.pop(context),
+              ),
+            ),
+            Divider(
+              height: 5,
+            ),
+            Ink(
+              color: Colors.grey[200],
+              child: ListTile(
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.red,
+                ),
+                title: Text(
+                  'Logout',
+                ),
+                onTap: () => logOut(context),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
